e696e34db1e7320db8e9715ef5526a052f3e5578
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-02-23 14:12:44 +0200

    more rubocop

--- spec/jpeg/cli_spec.rb
+++ spec/jpeg/cli_spec.rb
@@ -5,20 +5,13 @@ require "zimg/cli"
 each_sample("jpeg/**/*.jpg") do |fname|
   bname = File.basename(fname)
   RSpec.describe bname do
-    around do |example|
-      @stdout = StringIO.new
-      orig_stdout, $stdout = $stdout, @stdout
-      example.run
-      $stdout = orig_stdout
-    end
-
     it "is readable by CLI" do
-      expect { ZIMG::CLI.new([fname]).run }.to_not raise_error
+      expect { ZIMG::CLI.new([fname]).run }.to output(/ECS/).to_stdout
     end
 
     it "is readable by CLI verbosely" do
       skip("SLOW") if bname == "black-6000x6000.jpg" && !ENV["SLOW"]
-      expect { ZIMG::CLI.new([fname, "-vvv"]).run }.to_not raise_error
+      expect { ZIMG::CLI.new([fname, "-vvv"]).run }.to output(/ECS/).to_stdout
     end
   end
 end
--- spec/zimg_spec.rb
+++ spec/zimg_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe ZIMG do
   it "has a version number" do
-    expect(ZIMG::VERSION).not_to be nil
+    expect(ZIMG::VERSION).not_to be_nil
   end
 end
